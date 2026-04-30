4f8c2c660a0cf5ce7fe5f898e962255f5b96084f
AWI21 <4d4m.wi@gmail.com>
2026-04-30 14:26:48 +0200

    Fix: auto-create data dir on startup

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,17 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 const chalk = require('chalk');
 
 const DB_PATH = path.join(__dirname, '../../data/wolfy.db');
 let db;
 
 function initDatabase() {
+  // Auto-create the data directory if it doesn't exist (required on Render)
+  const dataDir = path.dirname(DB_PATH);
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+  }
   db = new Database(DB_PATH);
   db.pragma('journal_mode = WAL');
   db.pragma('foreign_keys = ON');
