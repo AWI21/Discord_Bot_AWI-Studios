536a5affe7f6898afcc4a5c71e4f65571246b2ae
AWI21 <4d4m.wi@gmail.com>
2026-07-31 19:43:18 +0200

    Fix advancedLogs import path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('./deploy-commands.js');
 const { startBot } = require('./src/bot');
 const { startWebServer } = require('./src/web/server');
-const registerAdvancedLogs = require('./src/events/advancedLogs');
+const registerAdvancedLogs = require('src/utils/advancedLogs.js');
 
 let client = null;
 
